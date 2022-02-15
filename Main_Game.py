30bac0d955de93495c07b3e14332835390051a83
Khu1027 <ahmadkk6858@gmail.com>
2022-02-15 19:52:54 +0000

    display the Statistics. Save and load the statistics from the text files Statistics.py

--- Main_Game.py
+++ Main_Game.py
@@ -2,6 +2,8 @@ import Variables
 import pygame
 import sys
 import New_Buttons
+import Statistics
+from datetime import datetime
 # -------------- Initialising Variables -------------
 pygame.init()
 
@@ -11,8 +13,27 @@ screen = Variables.screen
 clock = Variables.clock
 buttons = New_Buttons
 
+# -------------- Statistic counters --------------------
+hunger = Statistics.hunger
+happiness = Statistics.happiness
+health = Statistics.health
+
 # Rn the game just draws text saying start
 Message = buttons.Button("This is where the main game will be", 200, 80, (550, 250))
+
+def digital_clock():
+    time_now = datetime.now()
+    current_time = time_now.strftime("%H:%M:%S")
+    d_clock = buttons.Button(current_time, 150, 50, (25, 25))
+    d_clock.draw()
+
+def display_stats():
+    hunger_bar = buttons.Button(f"Hunger = {hunger}", 200, 50, (350, 25))
+    happiness_bar = buttons.Button(f"Happiness = {happiness}", 200, 50, (575, 25))
+    health_bar = buttons.Button(f"Health = {health}", 200, 50, (800, 25))
+    hunger_bar.draw()
+    happiness_bar.draw()
+    health_bar.draw()
 
 def display_screen():
     running = True
@@ -26,6 +47,9 @@ def display_screen():
                     running = False
 
         screen.fill(Variables.matcha)
+
         Message.draw_text()
+        digital_clock()
+        display_stats()
         pygame.display.flip()
         clock.tick(60)
---
+++ Statistics.py
@@ -0,0 +1,70 @@
+import json
+import New_Buttons
+buttons = New_Buttons
+
+def save_count(count):
+    with open(f"{count}.txt") as count_file:
+        json.dump(count, count_file)
+
+def load_count(count):
+    with open(f"{count}.txt") as count_file:
+        new_count = json.load(count_file)
+    return new_count
+
+
+# All of these try, except statements checks if the file exists.
+# If the file does exist it reassigns the variable the value that is in the file
+# If the file does not exist it assigns it a value, then makes a new file and saves the data
+# ------------- hunger -----------------
+try:
+    with open("hunger.txt") as hunger_file:
+        hunger = json.load(hunger_file)
+except:
+    hunger = 0
+    with open("hunger.txt", "w") as hunger_file:
+        json.dump(hunger, hunger_file)
+
+# ------------- happiness -----------------
+try:
+    with open("happiness.txt") as happiness_file:
+        happiness = json.load(happiness_file)
+except:
+    happiness = 0
+    with open("happiness.txt", "w") as happiness_file:
+        json.dump(happiness, happiness_file)
+
+# ------------- health -----------------
+try:
+    with open("health.txt") as health_file:
+        health = json.load(health_file)
+except:
+    health = 0
+    with open("health.txt", "w") as health_file:
+        json.dump(health, health_file)
+
+# ------------- play -----------------
+try:
+    with open("play.txt") as play_file:
+        play = json.load(play_file)
+except:
+    play = 0
+    with open("play.txt", "w") as play_file:
+        json.dump(play, play_file)
+
+# ------------- wash -----------------
+try:
+    with open("wash.txt") as wash_file:
+        wash = json.load(wash_file)
+except:
+    wash = 0
+    with open("wash.txt", "w") as wash_file:
+        json.dump(wash, wash_file)
+
+# ------------- feed -----------------
+try:
+    with open("feed.txt") as feed_file:
+        feed = json.load(feed_file)
+except:
+    feed = 0
+    with open("feed.txt", "w") as feed_file:
+        json.dump(feed, feed_file)
